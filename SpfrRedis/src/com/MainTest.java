bb9d3f66b746a4a80dbfcb776f669b6aa5300d25
xiaohong.dong <xiaohong.dong@htouhui.com>
2017-11-28 12:44:30 +0800

    redis测试连接

--- SpfrRedis/src/com/MainTest.java
+++ SpfrRedis/src/com/MainTest.java
@@ -1,5 +1,15 @@
 package com;
 
-public class MainTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"spring-redis.xml"})
+public class MainTest {
+	@Test
+	public void testRedisConnection(){
+		
+	}
 }
