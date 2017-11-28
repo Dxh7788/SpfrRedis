34885f2ff2858345014b2ca3aaa76b9667d72a8f
xiaohong.dong <xiaohong.dong@htouhui.com>
2017-11-28 13:03:16 +0800

    测试redis的一个版本

--- SpfrRedis/src/com/MainTest.java
+++ SpfrRedis/src/com/MainTest.java
@@ -2,14 +2,21 @@ package com;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"spring-redis.xml"})
 public class MainTest {
+	
+	@Autowired
+	RedisTemplate<String, String> template;
+	
 	@Test
 	public void testRedisConnection(){
-		
+		//操作字符串
+//		template.opsForValue().set("dxh_test_redis_str", "123");
 	}
 }
